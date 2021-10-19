702714606bf7877edcecfa86fb4d09d60c02cb32
Samuel Brehm <samuelbrehm@gmail.com>
2021-10-19 15:50:27 -0300

    Initial config AuthManager singin

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -1,8 +1,27 @@
-//
-//  AuthManager.swift
-//  SpotifyClone
-//
-//  Created by Samuel Brehm on 19/10/21.
-//
-
 import Foundation
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    var isSignedIn: Bool {
+        return false
+    }
+    
+    private var accessToken: String? {
+        return nil
+    }
+    
+    private var refreshToken: String? {
+        return nil
+    }
+    
+    private var tokenExpirationDate: Date? {
+        return nil
+    }
+    
+    private var shouldRefreshToken: Bool {
+        return false
+    }
+}
