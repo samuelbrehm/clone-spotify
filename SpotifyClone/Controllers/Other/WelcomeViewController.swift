b6f7dd38c3d8538e36cf4ed9cf6337e3c1b73cfa
Samuel Brehm <samuelbrehm@gmail.com>
2021-10-20 15:53:10 -0300

    Create authentication calls API Spotify

--- SpotifyClone/Controllers/Other/WelcomeViewController.swift
+++ SpotifyClone/Controllers/Other/WelcomeViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In with Spotfy", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Spotify"
         view.backgroundColor = .systemGreen
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        signInButton.frame = CGRect(
+            x: 20,
+            y: view.height - 60 - view.safeAreaInsets.bottom,
+            width: view.width - 50,
+            height: 50)
+    }
+    
+    @objc func didTapSignIn() {
+        let vc = AuthViewController()
+        vc.completionHandler = { [weak self] success in
+            DispatchQueue.main.async {
+                self?.handleSignIn(success: success)
+            }
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handleSignIn(success: Bool) {
+        
     }
 }
--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -3,6 +3,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "49f6f019b34a4a5c94711f5718618815"
+        static let clientSecret = "fd9042817bf840e9a826a410010ef821"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+    }
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://samuel-brehm.netlify.app"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
+    
     private init() {}
     
     var isSignedIn: Bool {
@@ -23,5 +37,58 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+        //Get token
+        guard let url = URL(string: Constants.tokenAPIURL) else { return }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "redirect_uri", value: "https://samuel-brehm.netlify.app")
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        
+        let basicToken = Constants.clientID + ":" + Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Failure to get base64")
+            completion(false)
+            return
+        }
+        
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("SUCCESS: \(json)")
+                completion(true)
+            } catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    //    public func refreshToken() {
+    //
+    //    }
+    
+    public func cacheToken() {
+        
     }
 }
