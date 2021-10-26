76967a97ff095e30cdce366a0c89a3bb1e8b612b
Samuel Brehm <samuelbrehm@gmail.com>
2021-10-26 17:29:00 -0300

    add images

--- SpotifyClone/Controllers/Other/ProfileViewController.swift
+++ SpotifyClone/Controllers/Other/ProfileViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ProfileViewController: UIViewController {
     
@@ -54,8 +55,26 @@ class ProfileViewController: UIViewController {
         self.models.append("Email Address: \(model.email)")
         self.models.append("User ID: \(model.id)")
         self.models.append("Plan: \(model.product)")
+        self.createTableHeader(with: model.images.first?.url)
         
         tableView.reloadData()
+    }
+    
+    private func createTableHeader(with string: String?) {
+        guard let urlString = string, let url = URL(string: urlString) else { return }
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.width / 1.5))
+        
+        let imageSize: CGFloat = headerView.height / 2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.contentMode = .scaleToFill
+        imageView.sd_setImage(with: url, completed: nil)
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageSize / 2
+        
+        tableView.tableHeaderView = headerView
     }
     
     private func failedGetProfile() {
